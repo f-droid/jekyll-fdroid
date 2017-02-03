8a302ec507394e422fb35e42c6bb0d72752e59bb
Nico Alt <nicoalt@posteo.org>
2017-02-03 14:25:38 +0100

    Fill front matter with information from hash
    TODO: Package array
    
    Related to #3.

--- lib/jekyll/FDroidPackageDetailPage.rb
+++ lib/jekyll/FDroidPackageDetailPage.rb
@@ -19,25 +19,51 @@ module Jekyll
 
 	class FDroidPackageDetailPage < Page
 		def initialize(site, base, package)
+			$package = package
 			@site = site
 			@base = base
 			@dir = "packages"
-			@name = package.at_xpath('id').content + "/index.html"
+			@name = $package.at_xpath('id').content + "/index.html"
 
 			self.process(@name)
 			self.read_yaml(File.join(base, '_layouts'), 'app.html')
-			self.data['title'] = package.at_xpath('name').content
-			self.data['beautifulURL'] = "/packages/" + package.at_xpath('id').content
-			xmlIcon = package.at_xpath('icon')
-			if xmlIcon != nil
-				self.data['icon'] = xmlIcon.content
+			getFrontMatterData
+		end
+
+		def getFrontMatterData
+			assignments = Hash.new
+			assignments = {
+				"added" => "added",
+				"antifeatures" => "antifeatures",
+				"bitcoin" => "bitcoin",
+				"categories" => "categories",
+				"changelog" => "changelog",
+				"description" => "desc",
+				"donate" => "donate",
+				"flattr" => "flattr",
+				"icon" => "icon",
+				"issueTracker" => "tracker",
+				"lastUpdated" => "lastupdated",
+				"license" => "license",
+				"suggestedVersionCode" => "marketvercode",
+				"suggestedVersion" => "marketversion",
+				"package" => "id",
+				"sourceCode" => "source",
+				"summary" => "summary",
+				"title" => "name",
+				"webSite" => "web"
+				}
+			assignments.each do |jekyll, xml|
+				addFrontMatterData(jekyll, xml)
 			end
-			self.data['package'] = package.at_xpath('id').content
-			self.data['summary'] = package.at_xpath('summary').content
-			self.data['description'] = package.at_xpath('desc').content
-			self.data['license'] = package.at_xpath('license').content
-			self.data['added'] = package.at_xpath('added').content
-			self.data['lastupdated'] = package.at_xpath('lastupdated').content
+			self.data["beautifulURL"] = "/packages/" + self.data["package"]
+		end
+
+		def addFrontMatterData(jekyll, xml)
+			data = $package.at_xpath(xml)
+			if data != nil
+				self.data[jekyll] = data.content
+			end
 		end
 	end
 end
