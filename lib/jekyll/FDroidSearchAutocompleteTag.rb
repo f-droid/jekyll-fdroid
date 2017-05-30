f8d2b92adb3f8cd6660cc629bc93d2425b3e0283
Peter Serwylo <peter@serwylo.com>
2017-05-30 16:42:51 +1000

    Add search widget.
    Provides a `{{ fdroid_search_autocomplete }}` and corresponding
    `{{ fdroid_search_autocomplete_with_template }}` tag for Liquid.
    The former has a hard coded senible template for each search result
    item. The later allows you to place a mustache.js template between
    the tags like:
    
    ```
    {{ fdroid_search_autocomplete_with_template }}
    Mustache.js template goes in here, and has access
    to the following tags:
     * {{ name }}
     * {{ summary }}
     * {{ icon }}
     * {{ packageName }}
    {{ endfdroid_search_autocomplete_with_template }}
    ```

---
+++ lib/jekyll/FDroidSearchAutocompleteTag.rb
@@ -0,0 +1,43 @@
+# F-Droid's Jekyll Plugin
+#
+# Copyright (C) 2017 Peter Serwylo
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module Jekyll
+
+	class FDroidSearchTemplateableAutocompleteBlock < Liquid::Block
+    def self.render_template(context, template)
+      context['result_item_template'] = template
+      context['template_id'] = rand(1000000)
+
+      template = Liquid::Template.parse(IO.read((File.expand_path "../../_layouts/search-autocomplete.html", File.dirname(__FILE__))))
+      template.render(context)
+    end
+
+    def render(context)
+      FDroidSearchTemplateableAutocompleteBlock.render_template(context, super.to_s)
+		end
+  end
+
+  class FDroidSearchAutocompleteTag < Liquid::Tag
+    def render(context)
+      result_item_template = IO.read((File.expand_path "../../_includes/search-autocomplete-default-result-template.html", File.dirname(__FILE__)))
+      FDroidSearchTemplateableAutocompleteBlock.render_template(context, result_item_template)
+    end
+  end
+end
+
+Liquid::Template.register_tag('fdroid_search_autocomplete', Jekyll::FDroidSearchAutocompleteTag)
+Liquid::Template.register_tag('fdroid_search_autocomplete_with_template', Jekyll::FDroidSearchTemplateableAutocompleteBlock)
