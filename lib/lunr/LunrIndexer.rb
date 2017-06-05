d644200f9a1fa9e1fccd442e0ec81d7ed410faf7
Peter Serwylo <peter@serwylo.com>
2017-06-06 09:53:26 +1000

    Remove unused config options and methods from LunrIndexer.
    Be conservative about refactoring, in the (perhaps vain?) hope that
    we could apply patches from upstream in the future.

--- lib/lunr/LunrIndexer.rb
+++ lib/lunr/LunrIndexer.rb
@@ -14,25 +14,9 @@ require 'v8'
 
 module Jekyll
   module LunrJsSearch
-    class Indexer < Jekyll::Generator
-      def initialize(config = {})
-        super(config)
-
-        lunr_config = {
-            'excludes' => [],
-            'strip_index_html' => false,
-            'min_length' => 3,
-            'stopwords' => 'stopwords.txt',
-            'fields' => {
-                'title' => 10,
-                'categories' => 20,
-                'tags' => 20,
-                'body' => 1
-            },
-            'js_dir' => 'js'
-        }.merge!(config['lunr_search'] || {})
-
-        @js_dir = lunr_config['js_dir']
+    class Indexer
+      def initialize()
+        @js_dir = 'js'
         gem_lunr = File.join(File.dirname(__FILE__), "../../build/lunr.min.js")
         @lunr_path = File.exist?(gem_lunr) ? gem_lunr : File.join(@js_dir, File.basename(gem_lunr))
         raise "Could not find #{@lunr_path}" if !File.exist?(@lunr_path)
@@ -41,21 +25,10 @@ module Jekyll
         ctx.load(@lunr_path)
         ctx['indexer'] = proc do |this|
           this.ref('id')
-          lunr_config['fields'].each_pair do |name, boost|
-            this.field(name, { 'boost' => boost })
-          end
         end
         @index = ctx.eval('lunr(indexer)')
         @lunr_version = ctx.eval('lunr.version')
         @docs = {}
-        @excludes = lunr_config['excludes']
-
-        # if web host supports index.html as default doc, then optionally exclude it from the url
-        @strip_index_html = lunr_config['strip_index_html']
-
-        # stop word exclusion configuration
-        @min_length = lunr_config['min_length']
-        @stopwords_file = lunr_config['stopwords']
       end
 
       # Index all pages except pages matching any value in config['lunr_excludes'] or with date['exclude_from_search']
@@ -122,34 +95,6 @@ module Jekyll
         end
       end
 
-      private
-
-      # load the stopwords file
-      def stopwords
-        @stopwords ||= IO.readlines(@stopwords_file).map { |l| l.strip }
-      end
-
-      def output_ext(doc)
-        if doc.is_a?(Jekyll::Document)
-          Jekyll::Renderer.new(@site, doc).output_ext
-        else
-          doc.output_ext
-        end
-      end
-
-      def pages_to_index(site)
-        items = []
-
-        # deep copy pages and documents (all collections, including posts)
-        site.pages.each {|page| items << page.dup }
-        site.documents.each {|document| items << document.dup }
-
-        # only process files that will be converted to .html and only non excluded files
-        items.select! {|i| i.respond_to?(:output_ext) && output_ext(i) == '.html' && ! @excludes.any? {|s| (i.url =~ Regexp.new(s)) != nil } }
-        items.reject! {|i| i.data['exclude_from_search'] }
-
-        items
-      end
     end
   end
 end
